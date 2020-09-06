9b9582a1fae06e89588cd47d7162e0eb9de71dde
claudy <nabab35000@gmail.com>
2020-09-06 19:38:42 +0200

    Ajoutez des fichiers projet.

---
+++ HelloTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Hello;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello world!", Program.createMessage());
+        }
+    }
+}
